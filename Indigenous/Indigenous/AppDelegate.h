9f26080d43c9ae556ac4c08fdd3ad68a7d4caeca
Alan Scarpa <alan.scarpa@gmail.com>
2015-07-28 16:52:41 -0400

    Creating workspace

---
+++ Indigenous/Indigenous/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Indigenous
+//
+//  Created by Alan Scarpa on 7/28/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Indigenous/Indigenous/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Indigenous
+//
+//  Created by Alan Scarpa on 7/28/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
