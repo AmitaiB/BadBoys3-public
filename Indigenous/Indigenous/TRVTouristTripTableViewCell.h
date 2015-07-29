e6fd83768273a5e79bd88d4c6dab0df3107430ac
dwicke01 <dwicke01@students.poly.edu>
2015-07-29 16:19:25 -0400

    putting my stuff on my own branch

---
+++ Indigenous/Indigenous/TRVTouristTripTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TRVTouristTripTableViewCell.h
+//  Indigenous
+//
+//  Created by Daniel Wickes on 7/29/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRVTouristTripTableViewCell : UITableViewCell
+
+@end
---
+++ Indigenous/Indigenous/TRVTouristTripTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TRVTouristTripTableViewCell.m
+//  Indigenous
+//
+//  Created by Daniel Wickes on 7/29/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import "TRVTouristTripTableViewCell.h"
+
+@implementation TRVTouristTripTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
