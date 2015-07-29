3b686c0ba9141e2f7fdceba30dd1ec9be0d8f738
leokwanbt14 <leokwanbt14@gmail.com>
2015-07-29 16:57:28 -0400

    added snippet custom cell to profile VC

--- Indigenous/Indigenous/TRVProfileViewController.m
+++ Indigenous/Indigenous/TRVProfileViewController.m
@@ -9,8 +9,11 @@
 #import "TRVProfileViewController.h"
 #import "TRVUser.h"
 #import "TRVBio.h"
+#import "TRVUserSnippetTableViewCell.h"
 
-@interface TRVProfileViewController ()
+@interface TRVProfileViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *profileTableView;
+@property (nonatomic, strong) TRVBio *currentBio;
 
 @end
 
@@ -18,19 +21,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.profileTableView.delegate = self;
+    self.profileTableView.dataSource = self;
     
-    TRVBio *currentBio = [[TRVBio alloc] initTouristWithUserName:@"leo" firstName:@"Leo" lastName:@"Kwan" email:@"leokwanbt14@gmail.com" phoneNumber:7188866958 profileImage:[UIImage imageNamed:@"leo.jpg"] bioDescription:@"great person" interests:[NSMutableArray arrayWithObjects:@"hi", @"hi", nil] language:@"English"];
+    
+    
+     self.currentBio = [[TRVBio alloc] initTouristWithUserName:@"leo" firstName:@"Leo" lastName:@"Kwan" email:@"leokwanbt14@gmail.com" phoneNumber:7188866958 profileImage:[UIImage imageNamed:@"leo.jpg"] bioDescription:@"great person" interests:[NSMutableArray arrayWithObjects:@"hi", @"hi", nil] language:@"English"];
     
     TRVUser *currentUser = [[TRVUser alloc] init];
-    currentUser.userBio = currentBio;
+    currentUser.userBio = self.currentBio;
     
-
-    // Do any additional setup after loading the view.
+       // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 2;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    if(indexPath.row == 0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"profileImageCell"];
+        return cell;
+    }
+    else if (indexPath.row == 1) {
+           TRVUserSnippetTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"userInfoSnippetCell"];
+        cell.nameLabel.text = self.currentBio.firstName;
+        cell.emailLabel.text = self.currentBio.email;
+        return cell;
+    }
+
+    return nil;
 }
 
 /*
---
+++ Indigenous/TRVUserSnippetTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TRVUserSnippetTableViewCell.m
+//  Indigenous
+//
+//  Created by Leo Kwan on 7/29/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import "TRVUserSnippetTableViewCell.h"
+
+@implementation TRVUserSnippetTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
