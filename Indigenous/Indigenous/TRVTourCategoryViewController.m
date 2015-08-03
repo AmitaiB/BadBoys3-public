47e2353e66ad205c395c720f7c32e874884f4c2c
leokwanbt14 <leokwanbt14@gmail.com>
2015-08-03 11:02:41 -0400

    got collection view to work

--- Indigenous/Indigenous/TRVTourCategoryViewController.m
+++ Indigenous/Indigenous/TRVTourCategoryViewController.m
@@ -10,6 +10,7 @@
 #import "TRVTourCategoryCollectionViewCell.h"
 
 @interface TRVTourCategoryViewController ()<UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+
 @property (weak, nonatomic) IBOutlet UICollectionView *categoryCollectionView;
 @property (nonatomic, strong) NSArray *tourCategoryViews;
 
@@ -23,52 +24,17 @@
     // make self as datasource and delegate
     self.categoryCollectionView.delegate =self;
     self.categoryCollectionView.dataSource = self;
-    self.categoryCollectionView.collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
     
     self.tourCategoryViews = @[@"newyork.jpg",@"beijing.jpg"];
     
-    
     [self.view addSubview:self.categoryCollectionView];
-
-    
-    
-    // Register cell classes
-    [self.categoryCollectionView registerClass:[TRVTourCategoryCollectionViewCell class] forCellWithReuseIdentifier:@"tourCategoryCollectionCell"];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-#pragma mark - collection view data source methods
-
-
-#pragma mark <UICollectionViewDataSource>
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 1;
-}
-
-
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    return CGSizeMake(50, 50);
-}
+#pragma mark - UICollectionViewDataSource
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.tourCategoryViews.count;
+//    return self.tourCategoryViews.count;
+    return 10;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -76,9 +42,30 @@
     TRVTourCategoryCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"tourCategoryCollectionCell" forIndexPath:indexPath];
     
 //    cell.categoryImageView.image = [UIImage imageNamed:[self.tourCategoryViews objectAtIndex:indexPath.row]];
-//    cell.categoryImageView.backgroundColor = [UIColor greenColor];
+    cell.categoryImageView.backgroundColor = [UIColor greenColor];
+
     return cell;
 }
+
+#pragma mark - UICollectionViewLayout
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(150, 150);
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 8.0;
+    
+    // NOT WORKING?
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 8.0;
+}
+
 
 
 @end
