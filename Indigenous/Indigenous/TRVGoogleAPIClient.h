3dd74c72aff16df8813ca12ec2b494a67754ff9a
AmitaiB <blickstein@gmail.com>
2015-08-07 07:10:33 -0400

    debugging xcode

---
+++ Indigenous/Indigenous/TRVGoogleAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  TRVGoogleAPIClient.h
+//  Indigenous
+//
+//  Created by Amitai Blickstein on 8/7/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRVGoogleAPIClient : NSObject
+
+@end
