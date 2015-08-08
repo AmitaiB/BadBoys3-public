9a63d4ea125d75ca9711362e8512ef7630c033c4
leokwanbt14 <leokwanbt14@gmail.com>
2015-08-08 12:24:10 -0400

    added category for dummy data

---
+++ Indigenous/NSMutableArray+TRVMutableArray_extraMethods.h
@@ -0,0 +1,17 @@
+//
+//  NSMutableArray+TRVMutableArray_extraMethods.h
+//  Indigenous
+//
+//  Created by Leo Kwan on 8/8/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TRVUser.h"
+
+
+@interface NSMutableArray (TRVMutableArray_extraMethods)
+
+-(NSMutableArray *) returnDummyAllTripsArrayForGuide:(TRVUser *)guide;
+
+@end
