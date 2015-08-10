62b3ba441abacb8fc9e7b59a378296d9f2a677fe
Alan Scarpa <alan.scarpa@gmail.com>
2015-08-10 16:54:49 -0400

    Adding stuff to parse

--- Indigenous/NSMutableArray+extraMethods.m
+++ Indigenous/NSMutableArray+extraMethods.m
@@ -12,6 +12,7 @@
 #import "TRVTourCategory.h"
 #import "TRVItinerary.h"
 #import <Parse/Parse.h>
+#import <malloc/malloc.h>
 
 @implementation NSMutableArray (TRVMutableArray_extraMethods)
 
@@ -74,16 +75,16 @@
 
 -(void)createParseDummyTour {
     
+    PFUser *currentUser = [PFUser currentUser];
     PFObject *theTour = [PFObject objectWithClassName:@"Tour"];
-    theTour[@"guideForThisTour"]= [PFUser currentUser];
-    
+    [theTour setObject:currentUser forKey:@"guideForThisTour"];
     
     PFObject *theItinerary = [PFObject objectWithClassName:@"Itinerary"];
     theTour[@"categoryForThisTour"] = @"Drink";
     theTour[@"tourDeparture"] = [NSDate dateWithTimeIntervalSinceNow:1000];
-    //  theTour[@"tourAverageRating"] = CGFLOAT;
-    
-    
+//    //  theTour[@"tourAverageRating"] = CGFLOAT;
+//    
+//    
     PFObject *theStop = [PFObject objectWithClassName:@"TourStop"];
     theTour[@"itineraryForThisTour"] = theItinerary;
     theItinerary[@"nameOfTour"] = @"Some name of tour";
@@ -91,34 +92,42 @@
     NSData *tourImageData = [NSData dataWithContentsOfFile:str];
     PFFile *tourImage = [PFFile fileWithName:@"tourImage" data:tourImageData];
     [tourImage save];
-    theItinerary[@"tourImage"] = tourImage;
+    //theItinerary[@"tourImage"] = tourImage;
     theItinerary[@"numberOfStops"] = @1;
     theItinerary[@"tourStops"] = @[theStop];
-    
-    // theItinerary[@"attractions"] = ARRAY OF ATTRACTIONS;
-    
+//
+////    // theItinerary[@"attractions"] = ARRAY OF ATTRACTIONS;
+//    
     theStop[@"operatorCost"] = @0;
     theStop[@"incidentalCost"] = @0;
     theStop[@"lat"] = @10;
     theStop[@"lng"] = @10;
-    //  theStop[@"tourStopLocation"] = pfgeopoint;
-    
-    //    PFObject *theMarker = [PFObject objectWithClassName:@"GMSMarker"];
-    //    theMarker[@"position"] = PFGEOPOINT;
-    //    theMarker[@"snippet"] = NSSTring;
-    //    theMarker[@"icon"] = UIImage;
-    //    theMarker[@"groundAnchor"] = cgpoint;
-    //    theMarker[@"infoWindowAnchor"] = cgpoint;
-    //
-    //    theStop[@"tourStopMarker"] = theMarker;
-    //
-    
-    
+////    //  theStop[@"tourStopLocation"] = pfgeopoint;
+////    
+////    //    PFObject *theMarker = [PFObject objectWithClassName:@"GMSMarker"];
+////    //    theMarker[@"position"] = PFGEOPOINT;
+////    //    theMarker[@"snippet"] = NSSTring;
+////    //    theMarker[@"icon"] = UIImage;
+////    //    theMarker[@"groundAnchor"] = cgpoint;
+////    //    theMarker[@"infoWindowAnchor"] = cgpoint;
+////    //
+////    //    theStop[@"tourStopMarker"] = theMarker;
+////    //
+////    
+
     [theTour saveInBackgroundWithBlock:^(BOOL success, NSError *error){
+        NSLog(@"THE TOUR ID IS: %@", theTour.objectId);
+       
         
-        if (error){
-            NSLog(@"The error saving tour is: %@", error);
-        }
+        [currentUser addObject:theTour.objectId forKey:@"myTrips"];
+        [currentUser saveInBackgroundWithBlock:^(BOOL success, NSError *error){
+            if (error){
+                NSLog(@"Cant save to array because: %@", error);
+            } else {
+                NSLog(@"Successfully added stuff to array.");
+            }
+        }];
+
         
     }];
     
