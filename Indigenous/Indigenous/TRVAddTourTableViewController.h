0ef94536111b41cbac84e2cb091ac10d8dfacd8d
AmitaiB <blickstein@gmail.com>
2015-08-15 22:47:42 -0400

    adds tableVC and navVC

---
+++ Indigenous/Indigenous/TRVAddTourTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TRVAddTourTableViewController.h
+//  Indigenous
+//
+//  Created by Amitai Blickstein on 8/15/15.
+//  Copyright (c) 2015 Bad Boys 3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRVAddTourTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *keyTourProperties;
+
+@end
