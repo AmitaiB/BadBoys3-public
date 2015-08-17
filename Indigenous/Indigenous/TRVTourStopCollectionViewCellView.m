61845964cb6a03eb94a546d8ad75f03ac501d9c3
dwicke01 <dwicke01@students.poly.edu>
2015-08-17 12:02:15 -0400

    committing before pull

--- Indigenous/Indigenous/TRVTourStopCollectionViewCellView.m
+++ Indigenous/Indigenous/TRVTourStopCollectionViewCellView.m
@@ -32,11 +32,38 @@
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-//- (void)drawRect:(CGRect)rect {
-//    // Drawing code
-//    self.backgroundColor = [UIColor greenColor];
-//    [super drawRect:rect];
-//}
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    [super drawRect:rect];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 1.0);
+    CGContextSetFillColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextBeginPath(context);
+    CGContextAddEllipseInRect(context, rect);
+    CGContextDrawPath(context, kCGPathFillStroke);
+    //CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
+    //CGRect smallerRect = CGRectInset(self.bounds, self.bounds.size.width/2, self.bounds.size.height/2);
+    //CGContextFillEllipseInRect(context, rect);
+    //CGContextStrokePath(context);
+
+//    var context = UIGraphicsGetCurrentContext()
+//    CGContextSetLineWidth(context, 2.0)
+//    CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+//    if drawIt == DrawIt.o {
+//        var rectangle = CGRectMake(4, 4, self.frame.width-8, self.frame.height-8)
+//        CGContextAddEllipseInRect(context, rectangle)
+//    }
+//    else if drawIt == DrawIt.x {
+//        var rectangle = CGRectMake(4, 4, self.frame.width-8, self.frame.height-8)
+//        CGContextMoveToPoint(context, rectangle.origin.x, rectangle.origin.y)
+//        CGContextAddLineToPoint(context, rectangle.width, rectangle.height)
+//        CGContextMoveToPoint(context, rectangle.origin.x + rectangle.width, rectangle.origin.y)
+//        CGContextAddLineToPoint(context, rectangle.origin.x, rectangle.origin.y + rectangle.height)
+//    }
+//    CGContextStrokePath(context)
+}
 
 
 @end
