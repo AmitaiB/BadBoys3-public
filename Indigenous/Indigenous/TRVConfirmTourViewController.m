ea4371c0b2bae9ea9dfd3e23b1f7a833d0b9e263
AmitaiB <blickstein@gmail.com>
2015-08-18 00:54:56 -0400

    cleaning up and tightening Confirm VC

--- Indigenous/Indigenous/TRVConfirmTourViewController.m
+++ Indigenous/Indigenous/TRVConfirmTourViewController.m
@@ -11,6 +11,8 @@
 #import <HNKGooglePlacesAutocomplete.h>
 #import <CLPlacemark+HNKAdditions.h>
 #import <RMSaveButton.h>
+#import "TRVConstants.h"
+#import <AddressBookUI/AddressBookUI.h>
 
 
 @interface TRVConfirmTourViewController ()
@@ -24,8 +26,6 @@ static NSString * const cellReuseID = @"cellReuseID";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
     self.tourNameLabel.text = self.tourObject.tourName;
     self.tourCategoryLabel.text = self.tourObject.tourCategory;
     self.tourAreaLabel.text/*TODO*/;
@@ -33,10 +33,6 @@ static NSString * const cellReuseID = @"cellReuseID";
     
     self.finalItineraryTableView.delegate = self;
     self.finalItineraryTableView.dataSource = self;
-    
-    self.isGeocoded = NO;
-    
-    
 }
 
 #pragma mark - TableviewDelegate and Datasource methods
@@ -52,142 +48,57 @@ static NSString * const cellReuseID = @"cellReuseID";
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellReuseID];
     
-//    if (self.isGeocoded) {
         cell.textLabel.text = ((HNKGooglePlacesAutocompletePlace*)self.tourObject.tourItinerary[indexPath.row]).name;
-//    } cell.textLabel.text = self.tourObject.tourItinerary;
-//    
     return cell;
 }
 
--(void)geocodeMe {
-    CLGeocoder *geocoder = [CLGeocoder new];
-    [geocoder geocodeAddressString:@"" completionHandler:^(NSArray *placemarks, NSError *error) {
-        
-        
-    }];
-    
-
-    [geocoder reverseGeocodeLocation:nil completionHandler:^(NSArray *placemarks, NSError *error) {
-        
-        
-    
-
-    /**
-     *  deprecated sample error message
-     */
-    if (error)
-    {
-//        [self.latitudeTextField setText:@"Not found"];
-//        [self.longitudeTextField setText:@"Not found"];
-//        
-//        UIAlertView *alert =
-//        [[UIAlertView alloc] initWithTitle:@"Geocoding Error"
-//                                   message:error.localizedDescription
-//                                  delegate:nil
-//                         cancelButtonTitle:@"OK"
-//                         otherButtonTitles: nil];
-//        
-//        [alert show];
-
-        
-        /**
-         *  deprecated sample success completion block
-         */
-        if ([placemarks count] > 0) {
-//            CLPlacemark *placemark = [placemarks lastObject];
-//            
-//            NSString *latString =
-//            [NSString stringWithFormat:@"%f",
-//             placemark.location.coordinate.latitude];
-//            
-//            [self.latitudeTextField setText:latString];
-//            
-//            NSString *longString =
-//            [NSString stringWithFormat:@"%f",
-//             placemark.location.coordinate.longitude];
-//            
-//            [self.longitudeTextField setText:longString];
-        }
-    }
-        }];
-        
-}
-
-        /*
-         // Override to support conditional editing of the table view.
-         - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-         // Return NO if you do not want the specified item to be editable.
-         return YES;
-         }
-         */
-        
-        /*
-         // Override to support editing the table view.
-         - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-         if (editingStyle == UITableViewCellEditingStyleDelete) {
-         // Delete the row from the data source
-         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-         } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-         }
-         }
-         */
-        
-        /*
-         // Override to support rearranging the table view.
-         - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-         }
-         */
-        
-        /*
-         // Override to support conditional rearranging of the table view.
-         - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-         // Return NO if you do not want the item to be re-orderable.
-         return YES;
-         }
-         */
-        
 
 //    ✅
--(void)createParseTourWithName:(NSString*)tourName category:(NSString*)tourCategory date:(NSDate*)departureDate image:(UIImage*)localTourImage itinerary:(NSArray*)arrayOfHNKLocations {
+-(void)createParseTourWithName:(NSString*)tourName
+                      category:(NSString*)tourCategory
+                          date:(NSDate  *)departureDate
+                         image:(UIImage *)localTourImage
+                     itinerary:(NSArray *)arrayOfHNKLocations {
 //    ✅
     PFUser *currentUser = [PFUser currentUser];
-    PFObject *theTour = [PFObject objectWithClassName:@"Tour"];
+    PFObject *theTour   = [PFObject objectWithClassName:@"Tour"];
     [theTour setObject:currentUser forKey:@"guideForThisTour"];
 //    ✅
-    PFObject *theItinerary = [PFObject objectWithClassName:@"Itinerary"];
-    theItinerary[@"nameOfTour"] = tourName;
-    theTour[@"categoryForThisTour"] = tourCategory;
-    theTour[@"tourDeparture"] = departureDate;
-    UIImage *tourImage = tourImage;
+    PFObject *theItinerary           = [PFObject objectWithClassName:@"Itinerary"];
+    theItinerary[@"nameOfTour"]      = tourName;
+    theTour[@"categoryForThisTour"]  = tourCategory;
+    theTour[@"tourDeparture"]        = departureDate;
+    UIImage *tourImage               = tourImage;
     theTour[@"itineraryForThisTour"] = theItinerary;
     
-    
-        //This gets the GooglePlace overload → NSStrings
-    NSMutableArray *arrayOfGenericAddresses;
+//    ✅
+        //This gets the GooglePlace overload, and makes it nice NSStrings
+     __block NSMutableArray *arrayOfPlacemarks;
         for (HNKGooglePlacesAutocompletePlace *place in arrayOfHNKLocations) {
-            [arrayOfGenericAddresses addObject:place.name];
+            [CLPlacemark hnk_placemarkFromGooglePlace:place
+                                               apiKey:GOOGLE_API_KEY2
+                                           completion:^(CLPlacemark *placemark, NSString *addressString, NSError *error) {
+                                               if (error) {
+                                                   NSLog(@"Error in CLPlacemark+hnk: %@", error.localizedDescription);
+                                               } else {
+                                                   [arrayOfPlacemarks addObject:placemark];
+                                               }
+                                           }];
         }
     
-    
-    NSMutableArray *tourStopsArray;
-    for (NSString *address in tourStopsArray) {
+    //    ✅
+        //Then we turn the placemarks into PFObjects!
+    for (CLPlacemark *placemark in arrayOfPlacemarks) {
         PFObject *theStop = [PFObject objectWithClassName:@"TourStop"];
-        /**
-         *  for later:
-//         */
-//                theStop[@"operatorCost"] = @0;
-//                theStop[@"incidentalCost"] = @0;
-//                theStop[@"lat"] = @10;
-//                theStop[@"lng"] = @10;
-//                theStop[@"coordinatePoint"] = [PFGeoPoint geoPointWithLatitude:10.0 longitude:10.0];
-        /**
-         *  important!
-         */
-        theStop[@"nameOfPlace"] = @"GEOCODE ME A REAL PLACE NAME";
+        theStop[@"operatorCost"]    = @0;
+        theStop[@"incidentalCost"]  = @0;
+        theStop[@"coordinatePoint"] = [PFGeoPoint geoPointWithLocation:placemark.location];
+//        theStop[@"lat"]             = placemark.location.coordinate.latitude;
+//        theStop[@"lng"]             = placemark.location.coordinate.longitude;
+        theStop[@"nameOfPlace"] = placemark.name; //e.g., Apple Inc.
+        theStop[@"addressOfEvent"] = ABCreateStringWithAddressDictionary(placemark.addressDictionary, NO);
             //TODO give the user a place to add notes to each stop.
         theStop[@"descriptionOfEvent"] = @"We will be running through the six with our woes.  You know how that goes.";
-        theStop[@"addressOfEvent"] = address;
         
             // MAKE SURE THAT THIS IS A PFFILE.   LOOK AT ABOVE CODE WHICH TAKES NSDATA AND CONVERTS TO PFFILE.
             // CURRENTLY USING PFIMAGE FROM LINE 18, BUT WE WANT A NEW STOP IMAGE.  REPEAT PROCESS FROM
@@ -199,10 +110,10 @@ static NSString * const cellReuseID = @"cellReuseID";
          *  @param .2f            <#.2f description#>
          *
          *  @return <#return value description#>
-        !!!NSData *imageData = UIImageJPEGRepresentation(localTourImage, .2f);
+         */
+        NSData *imageData = UIImageJPEGRepresentation(tourStopImage, .2f);
         PFFile *PFImage = [PFFile fileWithName:theItinerary[@"nameOfTour"] data:imageData];
          theStop[@"image"] = PFImage;
-         */
 
         [tourStopsArray addObject:theStop];
     } //Loop ends
@@ -253,6 +164,44 @@ static NSString * const cellReuseID = @"cellReuseID";
     }];
     
 }
+
+
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+
 /*
 #pragma mark - Navigation
 
